3fb3e0c5630749aed44cea7de7d0e8ecb2445060
Mark Theunissen <mark.theunissen@gmail.com>
2013-04-04 11:15:01 -0500

    Adding vagrant

---
+++ vagrant/provision/ansible-setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Simple function to exit with a message in the case of failure.
+function error_exit
+{
+  echo ''
+  echo "$1" 1>&2
+  exit 1
+}
+
+echo ''
+echo '======================================================================='
+echo 'Setting up Ansible...'
+
+if [ `which ansible` ]; then
+  echo 'Ansible already installed.'
+  exit 0
+fi
+
+echo ''
+echo '======================================================================='
+echo 'Updating apt and installing Ansible dependencies...'
+aptitude -q=2 update > /dev/null || error_exit "Unable to update apt cache."
+aptitude -q=2 -y install build-essential git python-dev \
+python-jinja2 python-yaml python-paramiko python-software-properties \
+python-pip debhelper python-support cdbs > /dev/null 2>&1 || error_exit "Unable to install required packages."
+
+echo ''
+echo '======================================================================='
+echo 'Checking out Ansible...'
+mkdir /usr/local/src/ansible || error_exit "Unable to create the directory for Ansible."
+git clone https://github.com/ansible/ansible.git /usr/local/src/ansible > /dev/null || error_exit "Unable to clone the git repository for Ansible."
+cd /usr/local/src/ansible
+git checkout v1.1 > /dev/null 2>&1 || error_exit "Unable to checkout the required tag."
+
+echo ''
+echo '======================================================================='
+echo 'Installing Ansible...'
+make install > /dev/null 2>&1 || error_exit "Unable to install Ansible."
+mkdir -p /etc/ansible
+echo "localhost" > /etc/ansible/hosts
+
+echo ''
+echo 'Finished Ansible setup.'
+echo ''
