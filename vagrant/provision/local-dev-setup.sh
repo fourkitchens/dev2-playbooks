aac91f1109b02d1be0bc74024eff301c24a688d3
Elliott Foster <elliottf@codebrews.com>
2013-09-27 15:44:12 -0500

    :mask: It's alive!

--- vagrant/provision/local-dev-setup.sh
+++ vagrant/provision/local-dev-setup.sh
@@ -3,5 +3,5 @@
 echo ''
 echo '======================================================================='
 echo 'Running local dev setup...'
-cd /home/vagrant/playbooks/config
-ansible-playbook --sudo -c local setup.yml
+cd /home/vagrant/playbooks
+ansible-playbook --sudo -c local site.yml
