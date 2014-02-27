fbf5490ad02ac05074e83f8049e03e268c1185a3
Peter Sieg <chasingmaxwell@gmail.com>
2014-02-27 11:09:43 -0500

    Put boris in a new role that only runs on local installs.

--- vagrant/provision/local-dev-setup.sh
+++ vagrant/provision/local-dev-setup.sh
@@ -5,3 +5,4 @@ echo '======================================================================='
 echo 'Running local dev setup...'
 cd /home/vagrant/playbooks
 ansible-playbook --sudo -c local site.yml
+ansible-playbook --sudo -c local vagrant.yml
