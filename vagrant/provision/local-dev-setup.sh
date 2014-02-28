ff38d9c59188c8bd0ac83f9a71a5184a06b7b173
Peter Sieg <chasingmaxwell@gmail.com>
2014-02-28 14:01:54 -0500

    Use groups in hosts file rather than a separate playbook to run local only roles.

--- vagrant/provision/local-dev-setup.sh
+++ vagrant/provision/local-dev-setup.sh
@@ -5,4 +5,3 @@ echo '======================================================================='
 echo 'Running local dev setup...'
 cd /home/vagrant/playbooks
 ansible-playbook --sudo -c local site.yml
-ansible-playbook --sudo -c local vagrant.yml
