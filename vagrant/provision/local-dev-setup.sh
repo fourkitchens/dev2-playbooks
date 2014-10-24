bf85f2c90ca246db06b83ccb6d32b782db2209c2
Ian Carrico <git@iancarrico.com>
2014-10-24 18:59:37 -0500

    Finish up a bunch of tasks

--- vagrant/provision/local-dev-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo ''
-echo '======================================================================='
-echo 'Running local dev setup...'
-cd /home/vagrant/playbooks
-ansible-playbook --sudo -c local site.yml
